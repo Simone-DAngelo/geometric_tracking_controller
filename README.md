6a7cc88ea95161d0d5d223e06f6f15286fda2386
Simone-DAngelo <simone.dang96@gmail.com>
2025-07-09 15:50:30 +0200

    stable version

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
 # lee_controller
 
-ros2 run lee_controller lee_controller --ros-args --params-file $(path_to_ros2_workspace)/src/lee_controller/conf/iris_param.yaml
+## Requirements
+- **PX4 Autopilot**: Ensure PX4 for SITL is installed and configured.
+- **Gazebo Classic**: Installed and properly configured.
+- **Micro XRCE-DDS Agent**: Installed to enable DDS communication.
+- **ROS2 Environment**: Installed and workspace properly configured.
+
+## Additional Help and Troubleshooting
+For further assistance or if you need help, refer to the [PX4 ROS2 User Guide](https://docs.px4.io/main/en/ros2/user_guide.html).
+
+With dockerfiles: https://github.com/Prisma-Drone-Team/sitl_utils
+
+## 1. Starting the PX4 SITL Simulation in Gazebo Classic
+1. Navigate to the PX4 SITL directory:
+    ```
+    cd /path/to/PX4-Autopilot
+    ```
+2. Set the topic to stream over ROS2 network modifying the file https://github.com/PX4/PX4-Autopilot/blob/main/src/modules/uxrce_dds_client/dds_topics.yaml
+
+3. Start the SITL simulation with the desired model:
+    ```
+    make px4_sitl gazebo-classic
+    ```
+    **Important Note for Gazebo Garden Users:**
+
+    If you are using Gazebo Garden instead of Gazebo Classic, be aware that the commands differ. Refer to the user guide for the specific instructions.
+
+## 2. Starting the Micro XRCE-DDS Agent
+1. Open a new terminal.
+2. Run the following command to start the agent (ensure that any required environment variables are set):
+    ```
+    MicroXRCEAgent udp4 -p 8888
+    ```
+3. Verify that the agent is running and listening on the configured port.
+
+## 3. Starting the Developed Node
+1. Build the project (if not already compiled):
+    ```
+    cd /home/dev/ros2_ws
+    colcon build --packages-select lee_controller
+    ```
+2. Source the environment:
+    ```
+    source install/setup.bash
+    ```
+3. Run the node:
+    ```
+    ros2 run lee_controller lee_controller --ros-args --params-file $(path_to_ros2_workspace)/src/lee_controller/conf/iris_param.yaml
+    ```
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,30 @@
 #include "rclcpp/rclcpp.hpp"
 #include "controller/lee_controller.h"
-#include "nav_msgs/msg/odometry.hpp"
+
 #include "boost/thread.hpp"
 #include <Eigen/Eigen>
-#include "std_msgs/msg/bool.hpp"
-#include "geometry_msgs/msg/wrench.hpp"
+
 #include "std_msgs/msg/float32_multi_array.hpp"
-#include "px4_msgs/msg/vehicle_local_position.hpp"
-#include "px4_msgs/msg/vehicle_attitude.hpp"
-#include "px4_msgs/msg/vehicle_angular_velocity.hpp"
+#include "px4_msgs/msg/vehicle_odometry.hpp"
 #include "px4_msgs/msg/vehicle_thrust_setpoint.hpp"
 #include "px4_msgs/msg/vehicle_torque_setpoint.hpp"
-#include "px4_msgs/msg/vehicle_rates_setpoint.hpp"
-#include <px4_msgs/msg/vehicle_command.hpp>
-#include "utils.h"
-#include <fstream>      // std::fstream
-#include <cstdio>
-using std::placeholders::_1;
+#include "px4_msgs/msg/vehicle_command.hpp"
+#include "px4_msgs/msg/offboard_control_mode.hpp"
 
+#include "utils.h"
+
+#include <cstdio>
+#include <chrono>
+
+using namespace std::chrono_literals;
 using namespace std;
 using namespace Eigen;
+
+using std::placeholders::_1;
 
 class CONTROLLER : public rclcpp::Node {
     public:
         CONTROLLER();
-        void LinPosVelCallback(const px4_msgs::msg::VehicleLocalPosition pos_msg);
-        void AttCallback(const px4_msgs::msg::VehicleAttitude att_msg);
-        void AngVelCallback(const px4_msgs::msg::VehicleAngularVelocity angvel_msg);
         void run();
         void ctrl_loop();
         void request_new_plan();
@@ -39,20 +37,20 @@ class CONTROLLER : public rclcpp::Node {
         void publish_vehicle_command(uint16_t command, float param1 = 0.0, float param2 = 0.0);
         void publish_thrust_setpoint(float thrust);
         void publish_torque_setpoint(Eigen::Vector3d torque);
+        void publish_offboard_control_mode();
         void timerCallback();
+
         rclcpp::TimerBase::SharedPtr timer_;
 
-        rclcpp::Subscription<px4_msgs::msg::VehicleLocalPosition>::SharedPtr _local_posvel_sub;
-        rclcpp::Subscription<px4_msgs::msg::VehicleAttitude>::SharedPtr _local_att_sub;
-        rclcpp::Subscription<px4_msgs::msg::VehicleAngularVelocity>::SharedPtr _local_angvel_sub;
+        rclcpp::Subscription<px4_msgs::msg::VehicleOdometry>::SharedPtr _odom_sub;
         
-        // rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr  _cmd_vel_motor_pub;  
         rclcpp::Publisher<px4_msgs::msg::VehicleThrustSetpoint>::SharedPtr _vehicle_thrust_sp_publisher; 
         rclcpp::Publisher<px4_msgs::msg::VehicleTorqueSetpoint>::SharedPtr _vehicle_torque_sp_publisher; 
-        rclcpp::Publisher<px4_msgs::msg::VehicleCommand>::SharedPtr	_vehicle_command_publisher;
-
+        rclcpp::Publisher<px4_msgs::msg::OffboardControlMode>::SharedPtr _offboard_control_mode_publisher;
+	    rclcpp::Publisher<px4_msgs::msg::VehicleCommand>::SharedPtr _vehicle_command_publisher;
 
         bool _first_odom, _new_plan;
+
         //---Parameters
         string _model_name;
         double _ctrl_rate;
@@ -84,10 +82,6 @@ class CONTROLLER : public rclcpp::Node {
         Eigen::Vector3d _perror;
         Eigen::Vector3d _verror;
 
-        px4_msgs::msg::VehicleLocalPosition _lin_posvel;
-        px4_msgs::msg::VehicleAttitude _att;
-        px4_msgs::msg::VehicleAngularVelocity _ang_vel;
-
         Vector3d _ref_p;
         Vector3d _ref_dp;
         Vector3d _ref_ddp;
@@ -117,7 +111,7 @@ class CONTROLLER : public rclcpp::Node {
 CONTROLLER::CONTROLLER() : Node("lee_controller"), _first_odom(false), _new_plan(false) {
     
     timer_ = this->create_wall_timer(
-        std::chrono::milliseconds(7),
+        std::chrono::milliseconds(10),
         std::bind(&CONTROLLER::timerCallback, this));
 
     // Get param from file yaml --------------------------------------- 
@@ -202,64 +196,24 @@ CONTROLLER::CONTROLLER() : Node("lee_controller"), _first_odom(false), _new_plan
     _ref_o_vel_max = _refo_vel_max;
     _ref_vel_max = _refvel_max;
 
-    // cout<<"-----------------DEBUG PARAMS: -------------\n"<<endl;
-    // cout<<"Model name: "<<_model_name<<endl;
-    // cout<<"ctrl_rate: "<<_ctrl_rate<<endl;
-    // cout<<"rate: "<<_rate<<endl;
-    // cout<<"Motor num: "<<_motor_num<<endl;
-    // cout<<"I: "<<_inertia<<endl;
-    // cout<<"K_p: "<<_position_gain.transpose()<<endl;
-    // cout<<"K_d: "<<_velocity_gain.transpose()<<endl;
-    // cout<<"K_r: "<<_attitude_gain.transpose()<<endl;
-    // cout<<"L_w: "<<_angular_rate_gain.transpose()<<endl;
-    // cout<<"mass: "<<_mass<<endl;
-    // cout<<"grav: "<<_gravity<<endl;
-    // cout<<"rotor angles: "<<endl;
-    // for(int i=0;i<4;i++){cout<<_rotor_angles[i]<<", ";}
-    // cout<<endl;
-    // cout<<"arm lenght: "<<endl;
-    // for(int i=0;i<4;i++){cout<<_arm_length[i]<<", ";}
-    // cout<<endl;
-    // cout<<"rot direction: "<<endl;
-    // for(int i=0;i<4;i++){cout<<_motor_rotation_direction[i]<<", ";}
-    // cout<<endl;
-    // cout<<"K_f: "<<_motor_force_k<<endl;
-    // cout<<"K_m_ "<<_motor_moment_k<<endl;
-    // cout<<"_ref_jerk_max: "<<_ref_jerk_max<<endl;
-    // cout<<"_ref_acc_max: "<<_ref_acc_max<<endl;
-    // cout<<"_ref_omega: "<<_ref_omega<<endl;
-    // cout<<"_ref_zita: "<<_ref_zita<<endl;
-    // cout<<"_ref_o_jerk_max: "<<_ref_o_jerk_max<<endl;
-    // cout<<"_ref_o_acc_max: "<<_ref_o_acc_max<<endl;
-    // cout<<"_ref_o_vel_max: "<<_ref_o_vel_max<<endl;
-    // cout<<"_ref_vel_max: "<<_ref_vel_max<<endl;
-    // ------------------------------------------------------------------
-
     // ROS2 pub/sub
     rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
 	auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
     
-    _local_posvel_sub = this->create_subscription<px4_msgs::msg::VehicleLocalPosition>(
-    "/fmu/vehicle_local_position/out", qos, [this](const px4_msgs::msg::VehicleLocalPosition::UniquePtr pos_msg) {
-        _mes_p << pos_msg->x, pos_msg->y, pos_msg->z;
-        _mes_dp << pos_msg->vx, pos_msg->vy, pos_msg->vz;
+    _odom_sub = this->create_subscription<px4_msgs::msg::VehicleOdometry>("/fmu/out/vehicle_odometry", qos, 
+    [this](const px4_msgs::msg::VehicleOdometry::SharedPtr msg) -> void {
+        _mes_p << msg->position[0], msg->position[1], msg->position[2];
+        _mes_dp << msg->velocity[0], msg->velocity[1], msg->velocity[2];
+        _att_q << msg->q[0], msg->q[1], msg->q[2], msg->q[3];
+        _omega_mes << msg->angular_velocity[0], msg->angular_velocity[1], msg->angular_velocity[2];
         _first_odom = true;
     });
-    _local_att_sub = this->create_subscription<px4_msgs::msg::VehicleAttitude>(
-    "/fmu/vehicle_attitude/out", qos, [this](const px4_msgs::msg::VehicleAttitude::UniquePtr att_msg) {
-        _att_q = Eigen::Vector4d( att_msg->q[0],att_msg->q[1],att_msg->q[2],att_msg->q[3] );
-        _Eta << utilities::quatToRpy (Eigen::Vector4d(att_msg->q[0],att_msg->q[1],att_msg->q[2],att_msg->q[3]));
-    });
-    _local_angvel_sub = this->create_subscription<px4_msgs::msg::VehicleAngularVelocity>(
-    "/fmu/vehicle_angular_velocity/out", qos, [this](const px4_msgs::msg::VehicleAngularVelocity::UniquePtr angvel_msg) {
-            _omega_mes << angvel_msg->xyz[0], angvel_msg->xyz[1], angvel_msg->xyz[2];
-    });
 
-    // _cmd_vel_motor_pub = this->create_publisher<std_msgs::msg::Float32MultiArray>(_model_name + "/cmd/motor_vel", 0);
-    _vehicle_thrust_sp_publisher = this->create_publisher<px4_msgs::msg::VehicleThrustSetpoint>("/fmu/vehicle_thrust_setpoint/in", 0);
-    _vehicle_torque_sp_publisher = this->create_publisher<px4_msgs::msg::VehicleTorqueSetpoint>("/fmu/vehicle_torque_setpoint/in", 0);
-    _vehicle_command_publisher = this->create_publisher<px4_msgs::msg::VehicleCommand>("/fmu/vehicle_command/in", 0);
-    //----------------
+    _vehicle_thrust_sp_publisher = this->create_publisher<px4_msgs::msg::VehicleThrustSetpoint>("/fmu/in/vehicle_thrust_setpoint", 0);
+    _vehicle_torque_sp_publisher = this->create_publisher<px4_msgs::msg::VehicleTorqueSetpoint>("/fmu/in/vehicle_torque_setpoint", 0);
+    _vehicle_command_publisher = this->create_publisher<px4_msgs::msg::VehicleCommand>("/fmu/in/vehicle_command", 0);
+    _offboard_control_mode_publisher = this->create_publisher<px4_msgs::msg::OffboardControlMode>("/fmu/in/offboard_control_mode", 0);
+
     _cmd_p << 0.0, 0.0, 0.0;
     _cmd_dp << 0.0, 0.0, 0.0;
     _cmd_ddp << 0.0, 0.0, 0.0;
@@ -276,64 +230,30 @@ CONTROLLER::CONTROLLER() : Node("lee_controller"), _first_odom(false), _new_plan
     _armed = false;
 }
 
-// void CONTROLLER::LinPosVelCallback(const px4_msgs::msg::VehicleLocalPosition pos_msg){
-//     _lin_posvel = pos_msg;
-//     _mes_p << _lin_posvel.x, _lin_posvel.y, _lin_posvel.z;
-//     _mes_dp << _lin_posvel.vx, _lin_posvel.vy, _lin_posvel.vz;
-//     _first_odom = true;
-// }
-
-// void CONTROLLER::AttCallback(const px4_msgs::msg::VehicleAttitude att_msg){
-//     _att = att_msg;
-//     _Eta << utilities::quatToRpy (Eigen::Vector4d(_att.q[0],_att.q[1],_att.q[2],_att.q[3]));
-// }
-
-// void CONTROLLER::AngVelCallback(const px4_msgs::msg::VehicleAngularVelocity angvel_msg){
-//     _ang_vel = angvel_msg;
-//     _omega_mes << _ang_vel.xyz[0], _ang_vel.xyz[1], _ang_vel.xyz[2];
-// }
-
-// void CONTROLLER::TrajSetCallback(const px4_msgs::msg::TrajectorySetpoint trajset_msg){
-//     _traj_set = trajset_msg;
-// }
-
 void CONTROLLER::timerCallback() {
     if(_first_odom){
-        // RCLCPP_INFO(this->get_logger(), "DEBUGGER");
-        // cout<<"\nLinear Pos: "<<_mes_p.transpose()<<"\n"<<endl;
-        // cout<<"\nLinear Vel: "<<_mes_dp.transpose()<<"\n"<<endl;
-        // cout<<"\nAttitude: "<<_Eta.transpose()<<"\n"<<endl;
-        // cout<<"\nQuaternion: "<<Eigen::Vector4d(_att.q[0],_att.q[1],_att.q[2],_att.q[3]).transpose()<<endl;
-        // cout<<"\nAng Vel: "<<_omega_mes.transpose()<<"\n"<<endl;
-        // RCLCPP_INFO(this->get_logger(), "--- END ---");
-        // cout<<endl;
         std_msgs::msg::Float32MultiArray motor_vel;
         motor_vel.data.resize( _motor_num );
     
         if(_cmd_p[2]<=-0.2 && !_armed){
-            // publish_vehicle_command(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 6);
+            publish_vehicle_command(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 6);
             arm();
             _armed = true;
         }
-        else if(_cmd_p[2]>-0.2 && _ref_p[2]>-0.2 && _armed && _new_plan){
-            disarm();
-            _armed = false;
-        }
+        // else if(_cmd_p[2]>-0.2 && _ref_p[2]>-0.2 && _armed && _new_plan){
+        //     disarm();
+        //     _armed = false;
+        // }
         if(_new_plan){
-            // for(int i=0; i<_motor_num; i++ ) {
-            //     motor_vel.data[i] =  _omega_motor(i);  
-            // }  
+            publish_offboard_control_mode();
             publish_thrust_setpoint(_thrust_normalized); 
             publish_torque_setpoint(_torque_normalized);
         }
         else{
-            // for(int i=0; i<_motor_num; i++ ) {
-            //     motor_vel.data[i] =  0.0;  
-            // }   
-            publish_thrust_setpoint(0.0); 
+            publish_offboard_control_mode();
+            publish_thrust_setpoint(0.1); 
             publish_torque_setpoint(Eigen::Vector3d(0.0, 0.0, 0.0));
         }
-        // _cmd_vel_motor_pub->publish( motor_vel );
         
     }
     else{
@@ -401,16 +321,29 @@ void CONTROLLER::publish_torque_setpoint(Eigen::Vector3d torque)
 	_vehicle_torque_sp_publisher->publish(msg);
 }
 
+void CONTROLLER::publish_offboard_control_mode()
+{
+    px4_msgs::msg::OffboardControlMode msg{};
+    msg.position = false;
+    msg.velocity = false;
+    msg.acceleration = false;
+    msg.body_rate = false;
+    msg.attitude = false;
+    msg.actuator = true;
+    msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+    _offboard_control_mode_publisher->publish(msg);
+}
+
 void CONTROLLER::request_new_plan() {
     float set_x, set_y, set_z, set_yaw;
     while(rclcpp::ok()) {
-        cout << "Insert new coordinates x (front), y (left), z (upword), yaw (c-clowise)" <<endl;
+        cout << "Insert new coordinates x (front), y (right), z (downword), yaw (clowise)" <<endl;
         scanf("%f %f %f %f", &set_x, &set_y, &set_z, &set_yaw);
         cout << "Request new plan for: [" << set_x << ", " << set_y << ", " << set_z << " - " << set_yaw << "]" << endl;
 
         //---ENU -> NED
-        _cmd_p << set_x, -set_y, -set_z;
-        _yaw_cmd = -set_yaw;
+        _cmd_p << set_x, set_y, set_z;
+        _yaw_cmd = set_yaw;
         //---
         _new_plan = true;
     }
@@ -470,7 +403,7 @@ void CONTROLLER::ffilter(){
     rclcpp::Rate r(_rate);
     double ref_T = 1.0/(double)_rate;
 
-    _cmd_p << _lin_posvel.x, _lin_posvel.y, _lin_posvel.z;
+    _cmd_p << _mes_p(0), _mes_p(1), _mes_p(2);
     _ref_p = _cmd_p;
 
     Vector3d ddp;
@@ -495,9 +428,6 @@ void CONTROLLER::ffilter(){
             
     while( rclcpp::ok() ) {
         ep = _cmd_p - _ref_p;
-
-        //cout << "_cmd_p: " << _cmd_p.transpose() << endl;
-        //cout << "_ref_f: " << _ref_p.transpose() << endl;
 
         double eyaw = _yaw_cmd - _ref_yaw;
 
@@ -586,8 +516,6 @@ void CONTROLLER::ctrl_loop() {
     Eigen::MatrixXd wd2rpm;
     
     while( !_first_odom ) usleep(0.1*1e6);
-    // _mes_p << _lin_posvel.x, _lin_posvel.y, _lin_posvel.z;
-    // _mes_dp << _lin_posvel.vx, _lin_posvel.vy, _lin_posvel.vz;
     
     if(!generate_allocation_matrix( allocation_M, _motor_num, _rotor_angles, _arm_length, _motor_force_k, _motor_moment_k, _motor_rotation_direction ) ) {     
         cout << "Wrong allocation matrix" << endl;
@@ -595,10 +523,7 @@ void CONTROLLER::ctrl_loop() {
     }
 
     // ----- max thrust and torque computation
-        _max_wrench[0] =  sin( _rotor_angles[0] ) * _arm_length[0] * _motor_force_k * _max_rot_speed[0] + sin( _rotor_angles[2] ) * _arm_length[2] * _motor_force_k * _max_rot_speed[2];
-        _max_wrench[1] =  cos( _rotor_angles[1] ) * _arm_length[1] * _motor_force_k * _max_rot_speed[2] + cos( _rotor_angles[2] ) * _arm_length[2] * _motor_force_k * _max_rot_speed[2];
-        _max_wrench[2] =  2 * _motor_force_k * _motor_moment_k * _max_rot_speed[0];
-        _max_wrench[3] = -4 * _motor_force_k * _max_rot_speed[0];
+        _max_wrench << 2.8198, 2.8198, 0.8480, 28.2656;
         cout<<"Vector of maximum command wrench: "<<_max_wrench.transpose()<<endl;
     // ---------------------------------------
 
@@ -620,18 +545,12 @@ void CONTROLLER::ctrl_loop() {
     Eigen::Vector4d ft;
     
     Vector3d att_err;
-    float alfa=0.0;
 
     while( rclcpp::ok() ) {
         //Measured    
-        // _mes_p << _lin_posvel.x, _lin_posvel.y, _lin_posvel.z;
-        // _mes_dp << _lin_posvel.vx, _lin_posvel.vy, _lin_posvel.vz;
         mes_q = _att_q;
-        mes_w = _omega_mes;  
-
-        
+        mes_w = _omega_mes;          
         Eigen::Matrix3d mes_R = utilities::QuatToMat( mes_q );
-        // mes_w = mes_R.transpose()*mes_w;
     
         lc.controller(_mes_p, _ref_p, mes_R, _mes_dp, _ref_dp, _ref_ddp, _ref_yaw, _ref_dyaw, mes_w, &ref_rotor_velocities, &ft, &_perror, &_verror, &att_err);   
         if(ft[3]<-_max_thrust){
@@ -640,32 +559,16 @@ void CONTROLLER::ctrl_loop() {
         if(_ref_p(2)>-0.05 && _cmd_p(2)>-0.3){
             ft<< 0,0,0,0;
         }
-        else{
-        alfa = alfa+0.002;
-        alfa = min(1.0f,alfa);
-        }
-        ft(3) = alfa*ft(3);
 
-        // RCLCPP_INFO(this->get_logger(),"Thrust Command: %lf",ft[3]);
-        _thrust_normalized = 0.0-((abs(ft[3])-0.0)/(_max_thrust-0)); //zi = (xi – min(x)) / (max(x) – min(x)) normalizzazione tra 0 e 1 (tra -1 e 0 fai il contrario)
-        // _torque_normalized << ft[0], ft[1], ft[2];
-        if(ft[0]<0.0){
-            _torque_normalized[0] = 0.0-(abs(ft[0])-0.0)/(abs(_max_wrench[0])-0.0);
-        }
-        else{
-            _torque_normalized[0] = (abs(ft[0])-0.0)/(abs(_max_wrench[0])-0.0);
-        }
-        if(ft[1]<0.0){
-            _torque_normalized[1] = 0.0-(abs(ft[1])-0.0)/(abs(_max_wrench[1])-0.0);
-        }
-        else{
-            _torque_normalized[1] = (abs(ft[1])-0.0)/(abs(_max_wrench[1])-0.0);
-        }
-        if(ft[2]<0.0){
-            _torque_normalized[2] = 0.0-(abs(ft[2])-0.0)/(abs(_max_wrench[2])-0.0);
-        }
-        else{
-            _torque_normalized[2] = (abs(ft[2])-0.0)/(abs(_max_wrench[2])-0.0);
+        _thrust_normalized = ft(3) / _max_wrench(3);
+        _thrust_normalized = std::clamp(_thrust_normalized, -1.0f, 0.0f);
+
+        _torque_normalized(0) = ft(0) / _max_wrench(0);
+        _torque_normalized(1) = ft(1) / _max_wrench(1);
+        _torque_normalized(2) = ft(2) / _max_wrench(2);
+
+        for(int i = 0; i < 3; i++) {
+            _torque_normalized(i) = std::clamp(_torque_normalized(i), -1.0, 1.0);
         }
 
         for(int i=0; i<_motor_num; i++ ) {
@@ -677,8 +580,6 @@ void CONTROLLER::ctrl_loop() {
 
 void CONTROLLER::run() {
     boost::thread ctrl_loop_t( &CONTROLLER::ctrl_loop, this );  
-    // auto node = std::make_shared<CONTROLLER>();
-    // rclcpp::spin(node);
 }
 
 int main(int argc, char **argv) {
